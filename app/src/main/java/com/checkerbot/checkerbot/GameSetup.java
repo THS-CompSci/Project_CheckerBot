f4a1e5ad76fe7fddd097a89d0a279d68394758d6
Damen <soccermandmaen@gmail.com>
2016-04-04 13:18:08 -0500

    GameSetup activity

---
+++ app/src/main/java/com/checkerbot/checkerbot/GameSetup.java
@@ -0,0 +1,13 @@
+package com.checkerbot.checkerbot;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GameSetup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_setup);
+    }
+}
