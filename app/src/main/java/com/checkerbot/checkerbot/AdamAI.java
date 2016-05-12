2ea1f06ba0562bb48b36a4231dd2418331ba0a52
Adam <adamhyche1@gmail.com>
2016-05-12 13:35:23 -0500

    HElLO

---
+++ app/src/main/java/com/checkerbot/checkerbot/AdamAI.java
@@ -0,0 +1,17 @@
+package com.checkerbot.checkerbot;
+
+/**
+ * Created by kyokyo on 5/12/2016.
+ */
+public class AdamAI extends Player{
+
+
+    public Square getTurn(Board board){
+        return null;
+    }
+
+    public Square getTurn(Square[] squares){
+        return null;
+    }
+
+}
