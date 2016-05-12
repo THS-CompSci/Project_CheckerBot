dbc42890f474e7f74fedfc22b7917a5970f8c5fb
Yasser7KX <y7kassifx@gmail.com>
2016-05-12 11:31:29 -0500

    Revert "Revert "Added example DamenAI.java""
    This reverts commit 9decc938e6dbde2f0ac1796e7cb1f5570374b9c0.

---
+++ app/src/main/java/com/checkerbot/checkerbot/DamenAI.java
@@ -0,0 +1,20 @@
+package com.checkerbot.checkerbot;
+
+/**
+ * Created by Damen on 5/11/2016.
+ */
+public class DamenAI extends Player {
+
+    public DamenAI(String player){
+        super(player);
+    }
+
+    public Square getTurn(Board board){
+        return null;
+    }
+
+    public Square getTurn(Square[] squares){
+        return null;
+    }
+
+}
