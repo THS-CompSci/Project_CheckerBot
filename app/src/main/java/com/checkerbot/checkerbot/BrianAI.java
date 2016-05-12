5b5d760744ac3c26edd8c8caad6ab668d86582d3
B-Cheung <bccomp2017@gmail.com>
2016-05-12 13:28:24 -0500

    BrianAI

---
+++ app/src/main/java/com/checkerbot/checkerbot/BrianAI.java
@@ -0,0 +1,20 @@
+package com.checkerbot.checkerbot;
+
+/**
+ * Created by Damen on 5/11/2016.
+ */
+public class BrianAI extends Player {
+
+    public BrianAI(String player){
+        super(player);
+    }
+
+    public Square getTurn(Board board){
+        return null;
+    }
+
+    public Square getTurn(Square[] squares){
+        return null;
+    }
+
+}
