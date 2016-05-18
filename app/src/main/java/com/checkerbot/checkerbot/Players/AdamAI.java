a76720a69365793cfd57ab90d6da45cd82164b86
Adam <adamhyche1@gmail.com>
2016-05-18 13:10:29 -0500

    Adam bot is SO RANDOM LOL

--- app/src/main/java/com/checkerbot/checkerbot/Players/AdamAI.java
+++ app/src/main/java/com/checkerbot/checkerbot/Players/AdamAI.java
@@ -3,27 +3,22 @@ package com.checkerbot.checkerbot.Players;
 import com.checkerbot.checkerbot.GameObjects.Board;
 import com.checkerbot.checkerbot.GameObjects.Player;
 import com.checkerbot.checkerbot.GameObjects.Square;
+import java.util.Random;
 
 /**
  * Created by kyokyo on 5/12/2016.
  */
 public class AdamAI extends Player {
 
-    //TODO: Eat food given to me by my master
-
     public Square getTurn(Board board){
-        Square[] s= board.getPieceArray(this);
-        for(Square t: s){
-            if(!board.getValidMoves(t,this).isEmpty()){
-                return t;
-            }
-        }
-        return null;
+        Square[] squares= board.getPieceArray(this);
+        Square s = squares[(int)(Math.random()*squares.length)];
+        return s;
     }
 
     public Square getTurn(Square[] squares){
-
-        return squares[0];
+        Square s = squares[(int)(Math.random()*squares.length)];
+        return s;
     }
 
 }
