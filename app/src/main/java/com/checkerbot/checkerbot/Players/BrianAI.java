e9fa21e22cdb9c4a87543c4f6672916359d0fc6c
B-Cheung <bccomp2017@gmail.com>
2016-05-20 14:39:54 -0500

    BrianAi

--- app/src/main/java/com/checkerbot/checkerbot/Players/BrianAI.java
+++ app/src/main/java/com/checkerbot/checkerbot/Players/BrianAI.java
@@ -1,5 +1,7 @@
 package com.checkerbot.checkerbot.Players;
 
+import android.graphics.Point;
+
 import com.checkerbot.checkerbot.GameObjects.Board;
 import com.checkerbot.checkerbot.GameObjects.Player;
 import com.checkerbot.checkerbot.GameObjects.Square;
@@ -9,12 +11,48 @@ import com.checkerbot.checkerbot.GameObjects.Square;
  */
 public class BrianAI extends Player {
 
+    int[][] boardMat = new int[8][8];
+
     public Square getTurn(Board board){
-        return null;
+        fillMatrix(board);
+
+        Square[] squares= board.getPieceArray(this);
+        while(true) {
+            Square s = squares[(int) (Math.random() * squares.length)];
+            if(!board.getValidMoves(s,this).isEmpty()){
+                return s;
+            }
+
+        }
+
     }
 
     public Square getTurn(Square[] squares){
-        return null;
+        Square s = squares[(int)(Math.random()*squares.length)];
+        return s;
     }
 
+
+    public void fillMatrix(Board board){
+        for(int r=0;r<8;r++){
+            for (int c=0;c<8;c++){
+                if(r%2==0 && c%2==1){
+                    if(board.getSquare(new Point(r,c)).getPiece().getColor() == this.getColor()){
+                        boardMat[r][c] = 0;
+                    }
+                    else {
+                        boardMat[r][c] = -2;
+                    }
+                }
+                else if (r%2==1 && c%2==0){
+                    if(board.getSquare(new Point(r,c)).getPiece().getColor() == this.getColor()){
+                        boardMat[r][c] = 0;
+                    }
+                    else {
+                        boardMat[r][c] = -2;
+                    }
+                }
+            }
+        }
+    }
 }
