45b4974f92c58743d6e39530c176104ce83fb2a3
Yasser7KX <y7kassifx@gmail.com>
2016-05-20 13:35:23 -0500

    Created YasserAI Package

--- app/src/main/java/com/checkerbot/checkerbot/Players/Yasser/Evaluation.java
+++ app/src/main/java/com/checkerbot/checkerbot/Players/Yasser/Evaluation.java
@@ -3,5 +3,17 @@ package com.checkerbot.checkerbot.Players.Yasser;
 /**
  * Created by Yasser on 5/20/2016.
  */
-public class Evaluation {
+public class Evaluation extends Heuristic {
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- app/src/main/java/com/checkerbot/checkerbot/Players/Yasser/MiniMax.java
+++ app/src/main/java/com/checkerbot/checkerbot/Players/Yasser/MiniMax.java
@@ -3,5 +3,5 @@ package com.checkerbot.checkerbot.Players.Yasser;
 /**
  * Created by Yasser on 5/20/2016.
  */
-public class MinMax {
+public class MiniMax {
 }
